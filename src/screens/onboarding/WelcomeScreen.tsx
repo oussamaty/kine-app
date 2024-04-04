e14d6af7d58fc97b9b489612907314f6b844e47d
Oussama Janaheddine <oussamaty62@gmail.com>
2024-04-04 22:30:55 +0200

    fix merge issues

--- src/screens/onboarding/WelcomeScreen.tsx
+++ src/screens/onboarding/WelcomeScreen.tsx
@@ -4,11 +4,6 @@ import { WelcomeScreenProp } from '@navigation/types';
 import KineLogo from '@assets/svg/kine_logo.svg';
 import FixedScreen from '@components/FixedScreen';
 import Button from '@components/Button';
-import { StyleSheet, Text, View } from 'react-native';
-import { WelcomeScreenProp } from '@navigation/types';
-import KineLogo from '@assets/svg/kine_logo.svg';
-import FixedScreen from '@components/FixedScreen';
-import Button from '@components/Button';
 
 const WelcomeScreen = ({ navigation }: WelcomeScreenProp) => {
 
